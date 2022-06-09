b61ff5a5d765ba9305b5a3f9b2195dbfcfcca1e7
YoussefJell <youssef.jellouli@yahoo.com>
2022-06-09 12:56:09 +0000

    added Utils

---
+++ utils.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+/**
+ * swap_ints - Swaps 2 integers in an array
+ * @a: First integer
+ * @b: Second integer
+ * Return: None (void)
+ */
+void swap_ints(int *a, int *b)
+{
+	int *tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
