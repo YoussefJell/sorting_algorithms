c35e219fcc400d9f552d711a468522867591810f
YoussefJell <youssef.jellouli@yahoo.com>
2022-06-12 22:03:22 +0000

    Fix checker

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 /**
+ * swap_ints - Swaps 2 integers in an array
+ * @a: First integer
+ * @b: Second integer
+ * Return: None (void)
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
  * selection_sort - selection_sorting algorithm
  *
  * @array: array to sort
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 /**
+ * swap_ints - Swaps 2 integers in an array
+ * @a: First integer
+ * @b: Second integer
+ * Return: None (void)
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
  * partition - Lomuto partition scheme
  *
  * @array: array to sort
